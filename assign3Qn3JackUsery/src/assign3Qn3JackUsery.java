2a5eea26b24233b4ee7b282e2c307acf3a09695f
Jack Usery <Jack.usery1@gmail.com>
2023-10-10 16:19:13 -0500

    initial commit

---
+++ assign3Qn3JackUsery/src/assign3Qn3JackUsery.java
@@ -0,0 +1,22 @@
+/**
+ *
+ *
+ * @author  Usery Jack
+ * @assignment CSCI 428 Assignment 3 Question 3
+ * @date 10/10/2023
+ */
+public class assign3Qn3JackUsery {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        }
+    }
+}

fbf380344d4485b4ebf08843231275ec2f6a3759
Jack Usery <83471718+JackUsery@users.noreply.github.com>
2023-11-06 11:28:32 -0600

    Created assignment module
    Created four assignment module and placed them in the Assignment 4 folder.

---
+++ Assignment 4/assign4Qn4JackUsery/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.assign4qn4jackusery {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.assign4qn4jackusery to javafx.fxml;
+    exports com.example.assign4qn4jackusery;
+}

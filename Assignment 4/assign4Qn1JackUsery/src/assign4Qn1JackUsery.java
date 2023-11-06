a917c11195b55a2acb779576ecae91e3f30103ad
Jack Usery <83471718+JackUsery@users.noreply.github.com>
2023-11-06 15:30:49 -0600

    Update assign4Qn1JackUsery.java
    Qn1. B
    
    - Implemented directorySize Method

--- Assignment 4/assign4Qn1JackUsery/src/assign4Qn1JackUsery.java
+++ Assignment 4/assign4Qn1JackUsery/src/assign4Qn1JackUsery.java
@@ -16,22 +16,21 @@ public class assign4Qn1JackUsery {
 
 
     public static void main(String[] args) {
-
-
+        
         // create a new scanner object
         Scanner input = new Scanner(System.in);
 
         // prompt the user to enter the directory name
-        System.out.print("Enter the File name: " );
+        System.out.print("Enter the directory name: " );
 
         // assign the user input to a string variable
-        String fileName = input.nextLine();
+        String dirName = input.nextLine();
 
         // create a new file object with the user input
-        File inputFile = new File(fileName);
+        File inputDir = new File(dirName);
 
         // check if the directory exists
-        if (inputFile.exists()) {
+        if (inputDir.exists()) {
             // display that the directory already exists
             System.out.println("Directory already exists");
             // exit the program
@@ -39,8 +38,28 @@ public class assign4Qn1JackUsery {
         } else {
             // create the directory
             System.out.println("Directory created successfully");
-            inputFile.mkdirs();
+            inputDir.mkdirs();
         }
-
     }
+
+    // directorySize method
+   public static int directorySize(File directory) {
+
+       int size = 0; // initialize size to 0
+
+       if (directory.isDirectory()) {
+           File[] files = directory.listFiles(); // create an array of files
+
+
+           for (int i = 0; i < files.length; i++) {
+
+               if (files[i].isFile()) {
+                   size++;
+               } else {
+                   size += directorySize(files[i]);
+               }
+           }
+       }
+       return size;
+   }
 }

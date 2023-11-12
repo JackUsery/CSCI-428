a23ce4d6c03e2fe72ad98f541cac3af985c987ef
Jack Usery <83471718+JackUsery@users.noreply.github.com>
2023-11-11 21:25:44 -0600

    Update assign4Qn1JackUsery
    The program prompts the user to enter a directory name, creates the directory, generates a dataset with 1,000 lines of employee data, measures the directory size, processes the data, and saves the results in "output.txt". The dataset includes first name, last name, rank, and randomly generated salaries for entry-level, middle-level, and senior software engineers.

--- Assignment 4/assign4Qn1JackUsery/src/assign4Qn1JackUsery.java
+++ Assignment 4/assign4Qn1JackUsery/src/assign4Qn1JackUsery.java
@@ -4,13 +4,12 @@
 
 
 
-
-
- */
+*/
 
 import java.io.*;
 import java.util.Random;
 import java.util.Scanner;
+import java.io.PrintWriter;
 
 public class assign4Qn1JackUsery {
 
@@ -18,60 +17,149 @@ public class assign4Qn1JackUsery {
 
     public static void main(String[] args) {
 
-        // create a new scanner object
-        Scanner input = new Scanner(System.in);
+        // Prompt the user to enter a directory name
+        System.out.print("Enter directory name: ");
+        Scanner scanner = new Scanner(System.in);
+        String directoryName = scanner.nextLine();
 
-        // prompt the user to enter the directory name
-        System.out.print("Enter the directory name: " );
+        // Create a new directory
+        boolean directoryCreated = createDirectory(directoryName);
 
-        // assign the user input to a string variable
-        String dirName = input.nextLine();
-
-
-
-        // create a new file object with the user input
-        File inputDir = new File(dirName);
-
-        int dirSize = directorySize(inputDir);
-
-        // check if the directory exists
-        if (inputDir.exists()) {
-            // display that the directory already exists
-            System.out.println("Directory already exists");
-            // exit the program
-            System.exit(1);
-        } else {
-            // create the directory
+        if (directoryCreated) {
             System.out.println("Directory created successfully");
-            inputDir.mkdirs();
+            // Measure the directory size
+            int fileSize = directorySize(directoryName);
+            System.out.println("Number of files in the directory: " + fileSize);
+
+            // Create a file to save a large dataset
+            String fileName = "input.txt";
+            generateDataset(directoryName, fileName);
+            System.out.println("Dataset created and saved as " + fileName);
+
+            String outputFileName = "output.txt";
+            processData(directoryName, fileName, outputFileName);
+        } else {
+            System.out.println("Directory already exists");
         }
     }
 
-    // directorySize method
-   public static int directorySize(File directory) {
+    // Create a new directory and return true if successful, false if it already exists
+    private static boolean createDirectory(String directoryName) {
+        File directory = new File(directoryName);
+        if (!directory.exists()) {
+            return directory.mkdirs();
+        }
+        return false;
+    }
 
-       int size = 0; // initialize size to 0
+    // Measure the directory size using the listFiles method
+    private static int directorySize(String directoryName) {
+        File directory = new File(directoryName);
+        File[] files = directory.listFiles();
+        if (files != null) {
+            return files.length;
+        }
+        return 0;
+    }
 
-       if (directory.isDirectory()) {
-           File[] files = directory.listFiles(); // create an array of files
+    // Generate a dataset and save it to a file
+    private static void generateDataset(String directoryName, String fileName) {
+        File directory = new File(directoryName);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
 
-           // loop through the files
-           for (int i = 0; i < files.length; i++) {
+        String filePath = directoryName + File.separator + fileName;
 
-               // check if the file is a file
-               if (files[i].isFile()) {
-                   size++;
-               } else {
-                   // if the file is a directory, recursively call the method
-                   size += directorySize(files[i]);
-               }
-           }
-       }
-       return size;
-   }
+        try (PrintWriter writer = new PrintWriter(filePath)) {
+            Random random = new Random();
 
-   // generateDataset method
-    public static void generateDataset(String fileName) {
+            for (int i = 1; i <= 1000; i++) {
+                String firstName = "FirstName" + i;
+                String lastName = "LastName" + i;
+                String rank = getRandomRank();
+                double salary = getRandomSalary(rank);
 
+                String line = firstName + " " + lastName + " " + rank + " $" + String.format("%.2f", salary);
+                writer.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Helper method to generate a random rank
+    private static String getRandomRank() {
+        String[] ranks = {"entry", "middle", "senior"};
+        Random random = new Random();
+        return ranks[random.nextInt(ranks.length)];
+    }
+
+    // Helper method to generate a random salary based on rank
+    private static double getRandomSalary(String rank) {
+        Random random = new Random();
+        switch (rank) {
+            case "entry":
+                return 40000 + random.nextDouble() * 20000;
+            case "middle":
+                return 40000 + random.nextDouble() * 110000;
+            case "senior":
+                return 75000 + random.nextDouble() * 225000;
+            default:
+                return 0;
+        }
+    }
+
+    private static void processData(String directoryName, String inputFileName, String outputFileName) {
+        double totalSalaryEntry = 0, totalSalaryMiddle = 0, totalSalarySenior = 0, totalSalaryAll = 0;
+        int countEntry = 0, countMiddle = 0, countSenior = 0, totalCount = 0;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(directoryName + File.separator + inputFileName));
+             PrintWriter writer = new PrintWriter(directoryName + File.separator + outputFileName)) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(" ");
+                String rank = parts[2];
+                double salary = Double.parseDouble(parts[3].substring(1)); // remove the "$" sign
+
+                switch (rank) {
+                    case "entry":
+                        totalSalaryEntry += salary;
+                        countEntry++;
+                        break;
+                    case "middle":
+                        totalSalaryMiddle += salary;
+                        countMiddle++;
+                        break;
+                    case "senior":
+                        totalSalarySenior += salary;
+                        countSenior++;
+                        break;
+                }
+
+                totalSalaryAll += salary;
+                totalCount++;
+            }
+
+            // Calculate average salaries
+            double averageSalaryEntry = countEntry > 0 ? totalSalaryEntry / countEntry : 0;
+            double averageSalaryMiddle = countMiddle > 0 ? totalSalaryMiddle / countMiddle : 0;
+            double averageSalarySenior = countSenior > 0 ? totalSalarySenior / countSenior : 0;
+            double averageSalaryAll = totalCount > 0 ? totalSalaryAll / totalCount : 0;
+
+            // Save results to output.txt
+            writer.println("Total Salary for Entry Level: $" + totalSalaryEntry);
+            writer.println("Average Salary for Entry Level: $" + averageSalaryEntry);
+            writer.println("Total Salary for Middle Level: $" + totalSalaryMiddle);
+            writer.println("Average Salary for Middle Level: $" + averageSalaryMiddle);
+            writer.println("Total Salary for Senior Level: $" + totalSalarySenior);
+            writer.println("Average Salary for Senior Level: $" + averageSalarySenior);
+            writer.println("Total Salary for All Employees: $" + totalSalaryAll);
+            writer.println("Average Salary for All Employees: $" + averageSalaryAll);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
